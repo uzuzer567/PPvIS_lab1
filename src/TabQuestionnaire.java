3ecf4aa3ebbb1b0f4b669308cb14b56f2242fded
uzuzer567 <49733302+uzuzer567@users.noreply.github.com>
2020-06-03 10:58:00 +0300

    Update TabQuestionnaire.java

--- src/TabQuestionnaire.java
+++ src/TabQuestionnaire.java
@@ -13,105 +13,101 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class TabQuestionnaire {
+    Boolean checkPersonInComboBox;
     Pane pane = new Pane();
+
     public Pane getPane() {
         tabQuestionnaire();
         return pane;
     }
-    Boolean checkPerson;
+
     public void tabQuestionnaire() {
-        List<Person> person = new ArrayList<>();
-        person.add(new Person("Алиса",  298510582));
-        person.add(new Person("Иван",  339736761));
-        person.add(new Person("Елизавета",  296810736));
+        List<Person> peopleInComboBox = new ArrayList<>();
+        peopleInComboBox.add(new Person("Алиса",  298510582));
+        peopleInComboBox.add(new Person("Иван",  339736761));
+        peopleInComboBox.add(new Person("Елизавета",  296810736));
 
         VBox vbox = new VBox();
-        HBox hbox1 = new HBox();
-        HBox hbox2 = new HBox();
-        HBox hbox3 = new HBox();
-        HBox hbox4 = new HBox();
-        HBox hbox5 = new HBox();
-        Text text = new Text();
-        TextField text1 = new TextField();
-        TextField text2 = new TextField();
-        Button button1 = new Button("Отправить");
-        Button button2 = new Button("Информация");
+        HBox hboxName = new HBox();
+        HBox hboxNumberPhone = new HBox();
+        HBox hboxButtonSendInformation = new HBox();
+        HBox hboxMessageAboutAddingInformation = new HBox();
+        HBox hboxComboBox = new HBox();
+        Text informationAboutSelectedPerson = new Text();
+        TextField name = new TextField();
+        TextField numberPhone = new TextField();
+        Button buttonAddInformationToComboBox = new Button("Отправить");
+        Button buttonGetInformationFromComboBox = new Button("Информация");
+        ComboBox<Person> comboBoxPersons = new ComboBox();
 
-        ComboBox<Person> comboBox1 = new ComboBox();
-        ComboBox<String> comboBox2 = new ComboBox();
-        ComboBox<String> comboBox3 = new ComboBox();
-
-        comboBox1.getItems().addAll(person);
-
+        comboBoxPersons.getItems().addAll(peopleInComboBox);
         pane.getChildren().add(vbox);
-
         vbox.setPadding(new Insets(10, 30, 10, 30));
         vbox.setSpacing(20);
-
         vbox.getChildren().add(new Text("Хочешь устоиться к нам? Заполни!"));
-        vbox.getChildren().add(hbox1);
+        vbox.getChildren().add(hboxName);
 
-        hbox1.setSpacing(20);
-        hbox1.getChildren().add(new Text("Имя:"));
-        hbox1.getChildren().add(text1);
-        vbox.getChildren().add(hbox2);
-        hbox2.getChildren().add(new Text("Контактный телефон:     "));
-        hbox2.getChildren().add(new Text("+375 "));
-        hbox2.getChildren().add(text2);
+        hboxName.setSpacing(20);
+        hboxName.getChildren().add(new Text("Имя:"));
+        hboxName.getChildren().add(name);
+        vbox.getChildren().add(hboxNumberPhone);
+        hboxNumberPhone.getChildren().add(new Text("Контактный телефон:     "));
+        hboxNumberPhone.getChildren().add(new Text("+375 "));
+        hboxNumberPhone.getChildren().add(numberPhone);
 
-        vbox.getChildren().add(hbox3);
-        hbox3.setSpacing(20);
-        hbox3.getChildren().add(button1);
+        vbox.getChildren().add(hboxButtonSendInformation);
+        hboxButtonSendInformation.setSpacing(20);
+        hboxButtonSendInformation.getChildren().add(buttonAddInformationToComboBox);
 
-        button1.setOnAction(new EventHandler<ActionEvent>() {
+        buttonAddInformationToComboBox.setOnAction(new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent event) {
-                hbox4.getChildren().clear();
-                checkPerson = false;
-                Person p = new Person(text1.getText(), Integer.parseInt(text2.getText()));
-                for (int i=0;i < person.size();i++) {
-                    if (person.get(i).equals(p)) {
-                        checkPerson = true;
+                hboxMessageAboutAddingInformation.getChildren().clear();
+                checkPersonInComboBox = false;
+                Person newPerson = new Person(name.getText(), Integer.parseInt(numberPhone.getText()));
+                for (int i=0; i < peopleInComboBox.size(); i++) {
+                    if (peopleInComboBox.get(i).equals(newPerson)) {
+                        checkPersonInComboBox = true;
                     }
 
                 }
-                if (!checkPerson){
-                    person.add(p);
-                    comboBox1.getItems().add(p);
-                    hbox4.getChildren().add(new Text("Вы занесены в базу! Мы c Вами свяжемся!"));
+                if (!checkPersonInComboBox){
+                    peopleInComboBox.add(newPerson);
+                    comboBoxPersons.getItems().add(newPerson);
+                    hboxMessageAboutAddingInformation.getChildren().add(new Text("Вы занесены в базу! Мы c Вами свяжемся!"));
                 }
-                else if (checkPerson){
-                    hbox4.getChildren().add(new Text("Такой человек уже есть в базе!"));
+                else if (checkPersonInComboBox){
+                    hboxMessageAboutAddingInformation.getChildren().add(new Text("Такой человек уже есть в базе!"));
                 }
-                text1.clear();
-                text2.clear();
+                name.clear();
+                numberPhone.clear();
             }
         });
 
-        vbox.getChildren().add(hbox4);
-        hbox4.setSpacing(20);
+        vbox.getChildren().add(hboxMessageAboutAddingInformation);
+        hboxMessageAboutAddingInformation.setSpacing(20);
         vbox.getChildren().add(new Text("База:"));
-        vbox.getChildren().add(hbox5);
-        hbox5.setSpacing(20);
-        hbox5.getChildren().add(comboBox1);
-        hbox5.getChildren().add(button2);
+        vbox.getChildren().add(hboxComboBox);
+        hboxComboBox.setSpacing(20);
+        hboxComboBox.getChildren().add(comboBoxPersons);
+        hboxComboBox.getChildren().add(buttonGetInformationFromComboBox);
 
-        button2.setOnAction(new EventHandler<ActionEvent>() {
+        buttonGetInformationFromComboBox.setOnAction(new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent event) {
-                hbox4.getChildren().clear();
-                Person pers = comboBox1.getSelectionModel().getSelectedItem();
-
-                if (pers != null) {
-                    text.setText("Имя: " + pers.getName() + ", " +
-                            "контактный телефон: +375" + pers.getNumber() + ".");
+                hboxMessageAboutAddingInformation.getChildren().clear();
+                Person selectedPerson = comboBoxPersons.getSelectionModel().getSelectedItem();
+                if (selectedPerson != null) {
+                    informationAboutSelectedPerson.setText("Имя: " + selectedPerson.getName() + ", " +
+                            "контактный телефон: +375" + selectedPerson.getNumber() + ".");
                 }
                 else {
-                    text.setText("Человек не выбран!");
+                    informationAboutSelectedPerson.setText("Человек не выбран!");
                 }
             }
         });
-        hbox5.getChildren().add(text);
+
+        hboxComboBox.getChildren().add(informationAboutSelectedPerson);
     }
 }
 
