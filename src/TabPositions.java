a800ac90cbed0c4517b8bb6458b87ee0c94e3949
uzuzer567 <49733302+uzuzer567@users.noreply.github.com>
2020-06-03 10:56:46 +0300

    Update TabPositions.java

--- src/TabPositions.java
+++ src/TabPositions.java
@@ -11,140 +11,140 @@ import javafx.scene.text.Text;
 
 public class TabPositions {
     Pane pane = new Pane();
+
     public Pane getPane() {
         tabPositions();
         return pane;
     }
 
     public void tabPositions() {
-        ToggleGroup toggleGroup = new ToggleGroup();
         VBox vbox = new VBox();
-        HBox hbox1 = new HBox();
-        HBox hbox2 = new HBox();
-        HBox hbox3 = new HBox();
-        TextField text = new TextField();
-        Button button = new Button("Информация");
-        Label selectedLabel = new Label();
+        HBox hboxTextNamePosition = new HBox();
+        HBox hboxNamePosition = new HBox();
+        HBox hboxFlowPane = new HBox();
+        TextField namePosition = new TextField();
+        Button buttonGetInformationAboutPosition = new Button("Информация");
+        Label selectedPosition = new Label();
 
-        RadioButton radioButton1 = new RadioButton("Директор ресторана");
-        RadioButton radioButton2 = new RadioButton("Первый ассистент директора");
-        RadioButton radioButton3 = new RadioButton("Менеджер участка");
-        RadioButton radioButton4 = new RadioButton("Менеджер по гостеприимству");
-        RadioButton radioButton5 = new RadioButton("Инструктор");
-        RadioButton radioButton6 = new RadioButton("Работник ресторана");
-        RadioButton radioButton7 = new RadioButton("Работник ночной смены");
-        RadioButton radioButton8 = new RadioButton("Бариста");
+        ToggleGroup toggleGroup = new ToggleGroup();
+        RadioButton radioButtonRestaurantDirector = new RadioButton("Директор ресторана");
+        RadioButton radioButtonFirstAssistantDirector = new RadioButton("Первый ассистент директора");
+        RadioButton radioButtonManagerSite = new RadioButton("Менеджер участка");
+        RadioButton radioButtonManagerHospitality = new RadioButton("Менеджер по гостеприимству");
+        RadioButton radioButtonInstructor = new RadioButton("Инструктор");
+        RadioButton radioButtonRestaurantWorker = new RadioButton("Работник ресторана");
+        RadioButton radioButtonNightWorker = new RadioButton("Работник ночной смены");
+        RadioButton radioButtonBarista = new RadioButton("Бариста");
+
+        radioButtonRestaurantDirector.setToggleGroup(toggleGroup);
+        radioButtonFirstAssistantDirector.setToggleGroup(toggleGroup);
+        radioButtonManagerSite.setToggleGroup(toggleGroup);
+        radioButtonManagerHospitality.setToggleGroup(toggleGroup);
+        radioButtonInstructor.setToggleGroup(toggleGroup);
+        radioButtonRestaurantWorker.setToggleGroup(toggleGroup);
+        radioButtonNightWorker.setToggleGroup(toggleGroup);
+        radioButtonBarista.setToggleGroup(toggleGroup);
 
         pane.getChildren().add(vbox);
-
         vbox.setPadding(new Insets(10, 30, 10, 30));
         vbox.setSpacing(20);
-        vbox.getChildren().add(hbox1);
-        hbox1.setSpacing(20);
-        hbox1.getChildren().add(new Text("Узнай больше о должностях!\n\nВведите название должности:"));
-        vbox.getChildren().add(hbox2);
-        hbox2.setSpacing(20);
-        hbox2.getChildren().add(text);
-        hbox2.getChildren().add(button);
+        vbox.getChildren().add(hboxTextNamePosition);
+        hboxTextNamePosition.setSpacing(20);
+        hboxTextNamePosition.getChildren().add(new Text("Узнай больше о должностях!\n\nВведите название должности:"));
+        vbox.getChildren().add(hboxNamePosition);
+        hboxNamePosition.setSpacing(20);
+        hboxNamePosition.getChildren().add(namePosition);
+        hboxNamePosition.getChildren().add(buttonGetInformationAboutPosition);
 
-        button.setOnAction(new EventHandler<ActionEvent>() {
-            @Override
+        buttonGetInformationAboutPosition.setOnAction(new EventHandler<ActionEvent>() {
             public void handle(ActionEvent event) {
-                boolean textHasBeenFound = false;
+                boolean positionHasBeenFound = false;
 
-                if (radioButton1.getText().equals(text.getText())) {
-                    radioButton1.setSelected(true);
-                    textHasBeenFound = true;
-                    selectedLabel.setText(" Вы выбрали: " + radioButton1.getText() + ".\n\n Директор ресторана ставит цели и задачи команде менеджеров \n ресторана и обеспечивает условия" +
+                if (radioButtonRestaurantDirector.getText().equals(namePosition.getText())) {
+                    radioButtonRestaurantDirector.setSelected(true);
+                    positionHasBeenFound = true;
+                    selectedPosition.setText(" Вы выбрали: " + radioButtonRestaurantDirector.getText() + ".\n\n Директор ресторана ставит цели и задачи команде менеджеров \n ресторана и обеспечивает условия" +
                             " для их выполнения. Он осуществляет \n контроль за удовлетворением потребностей гостей ресторана,\n" +
                             " развитием сотрудников и обеспечивает прибыльность ведения бизнеса.");
-                    text.clear();
+                    namePosition.clear();
                 }
 
-                if (radioButton2.getText().equals(text.getText())) {
-                    radioButton2.setSelected(true);
-                    textHasBeenFound = true;
-                    selectedLabel.setText(" Вы выбрали: " + radioButton2.getText() + ".\n\n Первый ассистент директора руководит одним или \n несколькими направлениями бизнеса в ресторане. Он" +
+                if (radioButtonFirstAssistantDirector.getText().equals(namePosition.getText())) {
+                    radioButtonFirstAssistantDirector.setSelected(true);
+                    positionHasBeenFound = true;
+                    selectedPosition.setText(" Вы выбрали: " + radioButtonFirstAssistantDirector.getText() + ".\n\n Первый ассистент директора руководит одним или \n несколькими направлениями бизнеса в ресторане. Он" +
                             " обладает лидерскими качествами,\n развивает команду работников и инструкторов, напрямую влияет на\n" +
                             " выполнение поставленных перед командой ресторана задач.");
-                    text.clear();
+                    namePosition.clear();
                 }
 
-                if (radioButton3.getText().equals(text.getText())) {
-                    radioButton3.setSelected(true);
-                    textHasBeenFound = true;
-                    selectedLabel.setText(" Вы выбрали: " + radioButton3.getText() + ".\n\n Менеджер участка организует работу одного из \n участков ресторана — зону сервиса, производства или" +
+                if (radioButtonManagerSite.getText().equals(namePosition.getText())) {
+                    radioButtonManagerSite.setSelected(true);
+                    positionHasBeenFound = true;
+                    selectedPosition.setText(" Вы выбрали: " + radioButtonManagerSite.getText() + ".\n\n Менеджер участка организует работу одного из \n участков ресторана — зону сервиса, производства или" +
                             " МакДрайв. Он контролирует \n точность выполнения процедур, скорость обслуживания, а также качество\n" +
                             " предоставляемой гостям продукции.");
-                    text.clear();
+                    namePosition.clear();
                 }
 
-                if (radioButton4.getText().equals(text.getText())) {
-                    radioButton4.setSelected(true);
-                    textHasBeenFound = true;
-                    selectedLabel.setText(" Вы выбрали: " + radioButton4.getText() + ".\n\n Менеджер по гостеприимству создает впечатляющую, \n гостеприимную атмосферу в ресторане: встречает" +
+                if (radioButtonManagerHospitality.getText().equals(namePosition.getText())) {
+                    radioButtonManagerHospitality.setSelected(true);
+                    positionHasBeenFound = true;
+                    selectedPosition.setText(" Вы выбрали: " + radioButtonManagerHospitality.getText() + ".\n\n Менеджер по гостеприимству создает впечатляющую, \n гостеприимную атмосферу в ресторане: встречает" +
                             " гостей, оказывает \n помощь в выборе заказа. Взаимодействует с гостями на протяжении их визита.\n" +
                             " Является примером заботы о гостях для работников ресторана.");
-                    text.clear();
+                    namePosition.clear();
                 }
 
-                if (radioButton5.getText().equals(text.getText())) {
-                    radioButton5.setSelected(true);
-                    textHasBeenFound = true;
-                    selectedLabel.setText(" Вы выбрали: " + radioButton5.getText() + ".\n\n Инструктор обучает новых и уже опытных сотрудников \n работе на участках. Он имеет отличные навыки" +
+                if (radioButtonInstructor.getText().equals(namePosition.getText())) {
+                    radioButtonInstructor.setSelected(true);
+                    positionHasBeenFound = true;
+                    selectedPosition.setText(" Вы выбрали: " + radioButtonInstructor.getText() + ".\n\n Инструктор обучает новых и уже опытных сотрудников \n работе на участках. Он имеет отличные навыки" +
                             " общения и работы в команде,\n демонстрирует высокий уровень работы на всех позициях.\n");
-                    text.clear();
+                    namePosition.clear();
                 }
 
-                if (radioButton6.getText().equals(text.getText())) {
-                    radioButton6.setSelected(true);
-                    textHasBeenFound = true;
-                    selectedLabel.setText(" Вы выбрали: " + radioButton6.getText() + ".\n\n Работник ресторана обслуживает гостей ресторана: \n принимает, собирает и выдает заказы. Работает на " +
+                if (radioButtonRestaurantWorker.getText().equals(namePosition.getText())) {
+                    radioButtonRestaurantWorker.setSelected(true);
+                    positionHasBeenFound = true;
+                    selectedPosition.setText(" Вы выбрали: " + radioButtonRestaurantWorker.getText() + ".\n\n Работник ресторана обслуживает гостей ресторана: \n принимает, собирает и выдает заказы. Работает на " +
                             "кухне.\n Поддерживает чистоту в зале и на прилегающей территории.");
-                    text.clear();
+                    namePosition.clear();
                 }
 
-                if (radioButton7.getText().equals(text.getText())) {
-                    radioButton7.setSelected(true);
-                    textHasBeenFound = true;
-                    selectedLabel.setText(" Вы выбрали: " + radioButton7.getText() + ".\n\n Работник ночной смены подготавливает производственные и \n служебные помещения ресторана" +
+                if (radioButtonNightWorker.getText().equals(namePosition.getText())) {
+                    radioButtonNightWorker.setSelected(true);
+                    positionHasBeenFound = true;
+                    selectedPosition.setText(" Вы выбрали: " + radioButtonNightWorker.getText() + ".\n\n Работник ночной смены подготавливает производственные и \n служебные помещения ресторана" +
                             " к открытию. Осуществляет комплексное \n обслуживание, моет и санитаризирует оборудование ресторана.");
-                    text.clear();
+                    namePosition.clear();
                 }
 
-                if (radioButton8.getText().equals(text.getText())) {
-                    radioButton8.setSelected(true);
-                    textHasBeenFound = true;
-                    selectedLabel.setText(" Вы выбрали: " + radioButton8.getText() + ".\n\n Бариста готовит кофе, напитки и десерты из \n ассортимента МакКафe, принимает, собирает и" +
+                if (radioButtonBarista.getText().equals(namePosition.getText())) {
+                    radioButtonBarista.setSelected(true);
+                    positionHasBeenFound = true;
+                    selectedPosition.setText(" Вы выбрали: " + radioButtonBarista.getText() + ".\n\n Бариста готовит кофе, напитки и десерты из \n ассортимента МакКафe, принимает, собирает и" +
                             " рассчитывает заказы на кассе.\n Создает гостеприимную обстановку для посетителей ресторана.\n Поддерживает" +
                             " чистоту на рабочем месте.");
-                    text.clear();
+                    namePosition.clear();
                 }
 
-                if (!textHasBeenFound){
+                if (!positionHasBeenFound){
                     Alert warning = new Alert(Alert.AlertType.ERROR);
                     warning.setContentText("Такого варианта нет!");
                     warning.show();
-                    text.clear();
+                    namePosition.clear();
                 }
             }
         });
 
-        radioButton1.setToggleGroup(toggleGroup);
-        radioButton2.setToggleGroup(toggleGroup);
-        radioButton3.setToggleGroup(toggleGroup);
-        radioButton4.setToggleGroup(toggleGroup);
-        radioButton5.setToggleGroup(toggleGroup);
-        radioButton6.setToggleGroup(toggleGroup);
-        radioButton7.setToggleGroup(toggleGroup);
-        radioButton8.setToggleGroup(toggleGroup);
-
         FlowPane flowPane = new FlowPane(Orientation.VERTICAL, 10, 10);
-        flowPane.getChildren().addAll(radioButton1, radioButton2, radioButton3, radioButton4, radioButton5, radioButton6, radioButton7, radioButton8);
+        flowPane.getChildren().addAll(radioButtonRestaurantDirector, radioButtonFirstAssistantDirector, radioButtonManagerSite, radioButtonManagerHospitality,
+                radioButtonInstructor, radioButtonRestaurantWorker, radioButtonNightWorker, radioButtonBarista);
         flowPane.setPadding(new Insets(10));
-        vbox.getChildren().add(hbox3);
-        hbox3.setSpacing(120);
-        hbox3.getChildren().add(flowPane);
-        hbox3.getChildren().add(selectedLabel);
+        vbox.getChildren().add(hboxFlowPane);
+        hboxFlowPane.setSpacing(120);
+        hboxFlowPane.getChildren().add(flowPane);
+        hboxFlowPane.getChildren().add(selectedPosition);
     }
 }
